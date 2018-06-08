11d66306a8d2258d7be7c10e9255545705d2eb7e
Frank Uhlig <uhlig.frank@gmail.com>
2018-06-08 10:55:54 +0200

    updating tests

--- tests/forward.cc
+++ tests/forward.cc
@@ -13,15 +13,12 @@ int main(int argc, char *argv[])
 
   brain.from_file(input_file);
 
-  const std::list<int> num_nodes {15, 10, 10, 1};
-  const std::list<int> layer_structure = brain.get_layer_structure();
-
-  assert(num_nodes == layer_structure);
-
   std::vector<double> one_pic(15, 1);
   std::list<std::vector<double>> input;
   input.push_back(one_pic);
 
-  brain.forward(input);
-  //calculate output value with something else and compare here
+  auto ener = brain.forward(input);
+  std::vector<double> expected(1, 0.);
+
+  assert( *(ener.begin()) == expected );
 }
