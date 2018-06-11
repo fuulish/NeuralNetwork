fe7a1dfb792a11ff121288a6fd38ec8b61ff878f
Frank Uhlig <uhlig.frank@gmail.com>
2018-06-11 07:44:29 +0200

    consistent indentation

--- src/NeuralNetwork.h
+++ src/NeuralNetwork.h
@@ -3,46 +3,48 @@
 #include <fstream>
 #include "Layer.h"
 
-class  NeuralNetwork {
+class NeuralNetwork
+{
 
-    public:
-        NeuralNetwork() : mean(0.), std(1.), num_layers(0) {}
-        void add_layer( int num_nodes );
-        void add_layer( int num_nodes, const char * nonlinearity );
-        void add_layer( int num_nodes, const std::string& nonlinearity );
-        void from_file( std::string filename );
-        void update_connectivity();
-        // const std::vector<double> forward( const std::vector<double>& activation );
-        std::list< std::vector<double> > forward( std::list< std::vector<double> > input );
+  public:
+    NeuralNetwork() : mean(0.), std(1.), num_layers(0) {}
+    void add_layer(int num_nodes);
+    void add_layer(int num_nodes, const char *nonlinearity);
+    void add_layer(int num_nodes, const std::string &nonlinearity);
+    void from_file(std::string filename);
+    void update_connectivity();
+    // const std::vector<double> forward( const std::vector<double>& activation );
+    std::list<std::vector<double>> forward(std::list<std::vector<double>> input);
 
-        void backward(/*some sort of loss function */);
+    void backward(/*some sort of loss function */);
 
-        void print();
+    void print();
 
-        int get_number_of_layers() { return num_layers; }
-        const std::list<int> get_layer_structure() {
-            std::list<int> layer_structure;
-            for( auto p : layers ) {
-                layer_structure.push_back(p.size());
-            }
-
-            return layer_structure;
-        }
-
-        void set_feature_standardization( double std, double mean )
+    int get_number_of_layers() { return num_layers; }
+    const std::list<int> get_layer_structure()
+    {
+        std::list<int> layer_structure;
+        for (auto p : layers)
         {
-            this->mean = mean;
-            this->std = std;
+            layer_structure.push_back(p.size());
         }
 
-    private:
-        std::list<Layer> layers;
-        double mean, std;
-        int num_layers;
+        return layer_structure;
+    }
 
-        // TODO: use smart_pointers, stupid
-        std::list< std::list<Layer*> > graph;
+    void set_feature_standardization(double std, double mean)
+    {
+        this->mean = mean;
+        this->std = std;
+    }
 
+  private:
+    std::list<Layer> layers;
+    double mean, std;
+    int num_layers;
+
+    // TODO: use smart_pointers, stupid
+    std::list<std::list<Layer *>> graph;
 };
 
 /* Q&A
