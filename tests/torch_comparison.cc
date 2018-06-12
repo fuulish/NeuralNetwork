eff394021d43407fdc53f4e3e1187962be026669
Frank Uhlig <uhlig.frank@gmail.com>
2018-06-12 11:31:30 +0200

    fixing TorchComparison tests

--- tests/torch_comparison.cc
+++ tests/torch_comparison.cc
@@ -49,12 +49,34 @@ int main( int argc, char *argv[] )
   ss_input << DATA_DIR << "torch_input.txt";
 
   std::stringstream ss_reference;
-  ss_input << DATA_DIR << "torch_reference.txt";
+  ss_reference << DATA_DIR << "torch_reference.txt";
 
   // TODO: get that magic number from somewhere
   std::list< std::vector<double> > input = read_from_txt( ss_input.str(), 64);
   std::list< std::vector<double> > output = brain.forward( input );
   std::list< std::vector<double> > reference = read_from_txt( ss_reference.str(), 1 );
 
-  assert( output == reference );
+  std::cout << input.size() << reference.size() << std::endl;
+
+  auto ref_iter = reference.begin();
+
+  int num_fails = 0;
+
+  for( auto out_iter = output.begin(); out_iter != output.end(); ++out_iter )
+  {
+    if( fabsf( (*out_iter)[0] - (*ref_iter)[0] ) > 1.e-6 )
+    {
+      std::cout << (*out_iter)[0] << " " << (*ref_iter)[0] << std::endl;
+      ++num_fails;
+    }
+    std::advance(ref_iter, 1);
+  }
+
+  if( num_fails ) {
+    std::cout << num_fails << std::endl;
+    return 1;
+  }
+  else {
+    return 0;
+  }
 }
