232cd30af398086f8f8d30cb28179abb7928b5e1
Frank Uhlig <uhlig.frank@gmail.com>
2018-09-03 20:47:05 +0200

    removing unnecessary variables

--- src/Layer.cc
+++ src/Layer.cc
@@ -14,7 +14,7 @@ void Layer::update_nodes(int num_following)
 const std::vector<double> Layer::compute_activation(const std::vector<double> &input)
 {
 
-  std::vector<double> activation(size(), 0.);
+  std::vector<double>& activation = cache;
 
   int cnt = 0;
   for (auto& node : nodes)
